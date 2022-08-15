0039b1c738e9640c7bc5b2e7671bb009f7f81dea
Dharma Rejesh <109216540+dharmarejesh@users.noreply.github.com>
2022-08-16 02:48:21 +0530

    Add files via upload

---
+++ AppAssignment3/ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Alexandra Abramov");
+        }
+    }
+}
---
+++ AppAssignment3/ConsoleApp3/Sum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace ConsoleApp3
+{
+     class Sum
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine(120 + 27);
+
+        }
+    }
+}
